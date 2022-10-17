24107f0e0d01bd0cb82d72cb970ecb17a3c52a6f
Dildeep <dildeepgill@hotmail.com>
2022-10-17 12:02:28 -0400

    fixed floating images

---
+++ app.js
@@ -0,0 +1,49 @@
+// service_fidtsqr
+// template_ydhmglg
+// BXG7G7ynHt0ghIQFn
+
+function contact(event){
+    event.preventDefault()
+    const loading= document.querySelector('.modal-loading')
+    const sent = document.querySelector('.modal-sent')
+    loading.classList.add("visable")
+
+    emailjs
+        .sendForm(
+            'service_fidtsqr',
+            'template_ydhmglg',
+            event.target,
+            'BXG7G7ynHt0ghIQFn'
+        ).then(() => {
+            loading.classList.remove("visable")
+            sent.classList.add("visable")
+        }).catch(() =>{
+            loading.classList.remove("visable")
+            alert("email is temporarily anavailable. Please contact me on Dildeepgill@hotmail.com")
+        })
+}
+
+
+function toggle(){
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle("open")
+    
+}
+
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const x= event.clientX / 30
+    const y= event.clientY / 30
+
+    for(let i = 0; i<shapes.length; i++ ){
+        const isOdd = i %2 !==0;
+        const oddInteger= isOdd? -1: 1;
+        shapes[i].style.transform=`translate(${x * oddInteger}px, ${y * oddInteger}px)`  
+        
+        
+    }
+}
+
+
+//
